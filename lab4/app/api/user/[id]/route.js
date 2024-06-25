91ccc69addcb06c0b8784827034a964170fe6595
bkung1 <650112418052@bru.ac.th>
2024-06-25 16:17:09 +0700

    lab 4

---
+++ lab4/app/api/user/[id]/route.js
@@ -0,0 +1,48 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+
+export async function GET (req ,{ params }){
+    const userID = Number (params.id);
+
+    const user = await prisma.user.findUnique({
+        where : {
+            id: userID,
+        },
+    });
+
+    return Response.json(user);
+}
+
+export async  function DELETE(req,{params}){
+
+    try {
+        const userID = Number (params.id)
+
+        const delUser = await prisma.user.delete({
+            where: { id: userID},
+        });
+        return Response.json(delUser,{ status: 200})
+        
+    } catch (error) {
+        return Response.json(error,{ status: 500}) 
+    }
+}
+
+export async function PUT(req,{params}){
+
+    try { 
+        const userID = Number (params.id);
+        const {email, name}= await req.json();
+        const updateUser = await prisma.user.update({
+        date :{
+            email,
+            name,
+        },
+    });     
+    return Response.json(updateUser,{status: 200});
+    } catch (error) {
+        return Response.json(error,{status: 500});    
+    } 
+}
---
+++ lab4/app/api/user/route.js
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient(
+)
+
+export async function GET (){
+    const allUesrs = await prisma.user.findMany()
+
+    return Response.json(allUesrs);
+}
+
+export async function POST(req){
+
+    try { 
+        const {email, name}= await req.json();
+        const createUser = await prisma.user.create({
+        date :{
+            email,
+            name,
+        },
+    });     
+    return Response.json(createUser,{status: 200});
+    } catch (error) {
+        return Response.json(error,{status: 500});    
+    } 
+}
